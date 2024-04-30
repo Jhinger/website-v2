3525bb21750ac7f42d8639095b86b2cef6f19397
Jhinger <gurshanjhinger22@gmail.com>
2024-04-29 21:10:13 -0700

    feat: repo design experience section

--- components/Experience/index.tsx
+++ components/Experience/index.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/utils/cn";
 import { data } from "@/data";
 import Link from "next/link";
 import ControlGroup from "@/components/ControlCenter/ControlGroup";
-import Image from "next/image";
+import RepoIcon from "@/assets/RepoIcon";
 
 type ExperienceProps = {
 	className?: string;
@@ -18,38 +18,9 @@ const Experience = ({ className }: ExperienceProps) => {
 			)}>
 			{data.experience.map((experience, index) => (
 				<ControlGroup
-					className="w-[20rem] md:w-[23rem] lg:w-[25rem] h-[12rem] relative overflow-hidden cursor-pointer first:ring-2 first:ring-primary first:ring-offset-2 ring-offset-colorBackground"
+					className="w-[20rem] md:w-[23rem] lg:w-[25rem] h-[10rem] relative overflow-hidden cursor-pointer first:ring-2 first:ring-primary first:ring-offset-2 ring-offset-colorBackground"
 					key={experience.name}>
-					<Image
-						src={experience.image}
-						fill={true}
-						sizes={experience.sizes}
-						className="relative"
-						style={{ objectFit: "cover" }}
-						alt={experience.name}
-						quality={100}
-						priority={true}
-					/>
-					<Link href={experience.url} target="__blank">
-						<div
-							className={cn(
-								"absolute left-0 top-0 w-full h-full bg-gradient-to-r opacity-100 md:opacity-0 hover:opacity-100 transition-opacity ease-in-out duration-200 from-black via-black/70"
-							)}>
-							<div className="flex flex-col justify-center items-start w-max h-max gap-2 relative left-8 top-6 text-xxsPlus text-white tracking-tighter">
-								<span className="font font-semibold text-sm">
-									{experience.name}
-								</span>
-								<div className="flex flex-col justify-center items-start tracking-normal">
-									<span className="text-xxsPlus font-medium">
-										{experience.position}
-									</span>
-									<span className="text-xxs font-light">
-										{experience.duration}
-									</span>
-								</div>
-							</div>
-						</div>
-					</Link>
+					<RepoIcon className="fill-white" />
 				</ControlGroup>
 			))}
 		</div>
