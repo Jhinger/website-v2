82afed4bbd3513c50722a564d75cb8ef85603985
Jhinger <gurshanjhinger22@gmail.com>
2024-05-29 19:35:38 -0700

    feat: speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import type { Metadata, Viewport } from "next";
 import Providers from "@/app/providers";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -39,6 +40,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<Providers>{children}</Providers>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
