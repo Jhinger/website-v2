e464bac468e49091c08a96ff8c810fbba1a2492a
Jhinger <gurshanjhinger22@gmail.com>
2024-10-26 14:36:42 -0700

    refactor: url mispelled

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://jhinger.com",
+      url: "https://www.jhinger.com/",
       lastModified: new Date(),
     },
     {
-      url: "https://jhingeer.com/resume.pdf",
+      url: "https://www.jhinger.com/resume.pdf",
       lastModified: new Date(),
     },
   ];
