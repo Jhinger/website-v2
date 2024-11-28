9e55697b40b1f147b7cbaa85c66fd31529424483
Jhinger <gurshanjhinger22@gmail.com>
2024-11-27 16:18:43 -0800

    refactor: spotify is lame asf

--- components/AudioPlayer/index.tsx
+++ components/AudioPlayer/index.tsx
@@ -1,91 +1,72 @@
-"use client";
-
 import ControlGroup from "@/components/ControlCenter/ControlButton";
 import Image from "next/image";
 import { cn } from "@/utils/cn";
-import { useRef, useState } from "react";
-import { PlayButton } from "@/assets";
 import { hoursAgo, minutesAgo } from "@/utils/time";
 import truncate from "@/utils/truncate";
 import type { RelevantSpotifyData } from "@/spotify";
 
 type AudioPlayerProps = RelevantSpotifyData & {
-	className?: string;
+  className?: string;
 };
 
 const AudioPlayer = ({
-	url,
-	image,
-	name,
-	artist,
-	played_at,
-	preview_url,
-	className,
+  url,
+  image,
+  name,
+  artist,
+  played_at,
+  preview_url,
+  className,
 }: AudioPlayerProps) => {
-	const [isPlaying, setIsPlaying] = useState(false);
-	const audioRef = useRef<HTMLAudioElement>(null);
+  const hrs = hoursAgo(played_at);
+  const min = minutesAgo(played_at);
 
-	const hrs = hoursAgo(played_at);
-	const min = minutesAgo(played_at);
+  const timeSince = () => {
+    if (hrs >= 24) {
+      return `${Math.round(hrs / 24)} day(s) ago`;
+    }
+    if (hrs > 0) {
+      return hrs === 1 ? `${hrs} hour ago` : `${hrs} hours ago`;
+    } else {
+      return min === 1 ? `${min} minute ago` : `${min} minutes ago`;
+    }
+  };
 
-	const timeSince = () => {
-		if (hrs >= 24) {
-			return `${Math.round(hrs / 24)} day(s) ago`;
-		}
-		if (hrs > 0) {
-			return hrs === 1 ? `${hrs} hour ago` : `${hrs} hours ago`;
-		} else {
-			return min === 1 ? `${min} minute ago` : `${min} minutes ago`;
-		}
-	};
-
-	const handleMusicPlaying = () => {
-		if (isPlaying) {
-			audioRef.current?.pause();
-			setIsPlaying(false);
-		} else {
-			audioRef.current?.play();
-			setIsPlaying(true);
-		}
-	};
-
-	return (
-		<div onClick={handleMusicPlaying} title={name}>
-			<ControlGroup
-				className={cn(
-					"relative w-[21rem] bg-group h-max p-0 overflow-hidden border-2 border-transparent",
-					className,
-					isPlaying && "animate-crazyColors"
-				)}>
-				<audio ref={audioRef} src={preview_url} loop></audio>
-				<div className="w-full flex justify-end">
-					<div className="relative w-[10rem]">
-						<Image
-							src={image}
-							className="flex-1 p-0 rounded-r-xl"
-							width={640}
-							height={640}
-							alt="Album Cover"
-							quality={100}
-							priority={true}
-						/>
-					</div>
-				</div>
-				<div className="absolute w-full h-full bg-gradient-to-r from-black via-black via-51"></div>
-				<div className="p-6 absolute w-2/3 h-full flex flex-col justify-between items-start left-0 top-0 right-0 text-xxsPlus text-gray-300">
-					<div className="flex flex-col gap-1 items-start">
-						<span className="font-semibold">Last Listened to</span>
-						<span className="font-light">{timeSince()}</span>
-					</div>
-					<div className="flex flex-col gap-1 items-start">
-						<span className="font-semibold">{truncate(name, 20)}</span>
-						<span className="font-light">{truncate(artist, 20)}</span>
-					</div>
-				</div>
-				{!isPlaying && <PlayButton className="w-4 h-4 z-100" />}
-			</ControlGroup>
-		</div>
-	);
+  return (
+    <div title={name}>
+      <ControlGroup
+        className={cn(
+          "relative w-[21rem] bg-group h-max p-0 overflow-hidden border-2 border-transparent",
+          className,
+        )}
+      >
+        <div className="w-full flex justify-end">
+          <div className="relative w-[10rem]">
+            <Image
+              src={image}
+              className="flex-1 p-0 rounded-r-xl"
+              width={640}
+              height={640}
+              alt="Album Cover"
+              quality={100}
+              priority={true}
+            />
+          </div>
+        </div>
+        <div className="absolute w-full h-full bg-gradient-to-r from-black via-black via-51"></div>
+        <div className="p-6 absolute w-2/3 h-full flex flex-col justify-between items-start left-0 top-0 right-0 text-xxsPlus text-gray-300">
+          <div className="flex flex-col gap-1 items-start">
+            <span className="font-semibold">Last Listened to</span>
+            <span className="font-light">{timeSince()}</span>
+          </div>
+          <div className="flex flex-col gap-1 items-start">
+            <span className="font-semibold">{truncate(name, 20)}</span>
+            <span className="font-light">{truncate(artist, 20)}</span>
+          </div>
+        </div>
+      </ControlGroup>
+    </div>
+  );
 };
 
 export default AudioPlayer;
--- spotify/index.ts
+++ spotify/index.ts
@@ -7,29 +7,31 @@ import type { ObjectFromUnion } from "@/types";
 export type RelevantSpotifyData = ObjectFromUnion<keyof typeof data.spotify>;
 
 async function getSpotifyAccessToken(): Promise<string | null> {
-  noStore(); 
+  noStore();
   return await kv.get("spotify_access_token");
 }
 
 async function getSpotifyRefreshToken(): Promise<string | null> {
-  noStore(); 
+  noStore();
   return await kv.get("spotify_refresh_token");
 }
 
-function parseSpotifyResponse(res: SpotifyApi.UsersRecentlyPlayedTracksResponse): RelevantSpotifyData {
+function parseSpotifyResponse(
+  res: SpotifyApi.UsersRecentlyPlayedTracksResponse,
+): RelevantSpotifyData {
   return {
-    url: res.items[0].track.external_urls.spotify, 
+    url: res.items[0].track.external_urls.spotify,
     preview_url: res.items[0].track.preview_url ?? "",
     image: res.items[0].track.album.images[0].url,
     name: res.items[0].track.name,
     artist: res.items[0].track.artists[0].name,
     played_at: res.items[0].played_at,
-  }
+  };
 }
 
 async function refreshTokens(): Promise<string | Error> {
-  const refresh_token = await getSpotifyRefreshToken() as string;
-  
+  const refresh_token = (await getSpotifyRefreshToken()) as string;
+
   try {
     const response = await fetch(process.env.SPOTIFY_TOKEN_ENDPOINT, {
       method: "POST",
@@ -38,19 +40,21 @@ async function refreshTokens(): Promise<string | Error> {
         refresh_token,
       }),
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Basic ${Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64')}`
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString("base64")}`,
       },
-      cache: "no-store"
-    })
+      cache: "no-store",
+    });
 
     if (response.ok) {
       const body = await response.json();
 
-      await kv.set("spotify_access_token", body.access_token, { ex: body.expires_in });
+      await kv.set("spotify_access_token", body.access_token, {
+        ex: body.expires_in,
+      });
       body.refresh_token
         ? await kv.set("spotify_refresh_token", body.refresh_token)
-        : null
+        : null;
 
       return body.access_token;
     }
@@ -61,11 +65,11 @@ async function refreshTokens(): Promise<string | Error> {
   }
 }
 
-async function getRecentlyPlayed(access_token: string | null | Error): Promise<RelevantSpotifyData> {
-  if (
-    access_token === null || 
-    access_token instanceof Error
-  ) return data.spotify;
+async function getRecentlyPlayed(
+  access_token: string | null | Error,
+): Promise<RelevantSpotifyData> {
+  if (access_token === null || access_token instanceof Error)
+    return data.spotify;
 
   const targetURL = new URL(process.env.SPOTIFY_RECENTLY_PLAYED_ENDPOINT);
   targetURL.searchParams.set("limit", "1");
@@ -74,17 +78,17 @@ async function getRecentlyPlayed(access_token: string | null | Error): Promise<R
     const response = await fetch(targetURL, {
       method: "GET",
       headers: {
-        'Authorization': `Bearer ${access_token}`
+        Authorization: `Bearer ${access_token}`,
       },
       next: {
-        revalidate: 300
-      }
-    })
+        revalidate: 300,
+      },
+    });
 
     if (!response.ok) {
       return data.spotify;
     }
-    
+
     return parseSpotifyResponse(await response.json());
   } catch (error) {
     return data.spotify;
@@ -94,7 +98,8 @@ async function getRecentlyPlayed(access_token: string | null | Error): Promise<R
 export default async function fetchSpotifyData(): Promise<RelevantSpotifyData> {
   if (!isProduction()) return data.spotify;
 
-  const access_token = await getSpotifyAccessToken() || await refreshTokens();
+  const access_token =
+    (await getSpotifyAccessToken()) || (await refreshTokens());
 
-  return await getRecentlyPlayed(access_token)
+  return await getRecentlyPlayed(access_token);
 }
